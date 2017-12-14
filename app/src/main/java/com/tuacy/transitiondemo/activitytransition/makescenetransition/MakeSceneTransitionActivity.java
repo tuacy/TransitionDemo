9db16278a062c840b3de9905d0000e796683a863
tuacy <1007178106@qq.com>
2017-12-14 22:29:05 +0800

    Activity过渡动画实例

--- app/src/main/java/com/tuacy/transitiondemo/activitytransition/makescenetransition/MakeSceneTransitionActivity.java
+++ app/src/main/java/com/tuacy/transitiondemo/activitytransition/makescenetransition/MakeSceneTransitionActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Fade;
 import android.transition.Slide;
+import android.transition.TransitionInflater;
 import android.transition.TransitionSet;
 import android.view.Gravity;
 
@@ -17,23 +18,19 @@ public class MakeSceneTransitionActivity extends AppCompatActivity {
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity_animation_to);
-//		TransitionSet transitionSet = new TransitionSet();
-//		Slide slideLeft = new Slide(Gravity.START);
-//		slideLeft.addTarget(R.id.image_one);
-//		transitionSet.addTransition(slideLeft);
-//		Slide slideRight = new Slide(Gravity.END);
-//		slideLeft.addTarget(R.id.image_two);
-//		transitionSet.addTransition(slideRight);
-//		Slide slideTop = new Slide(Gravity.TOP);
-//		slideTop.addTarget(R.id.image_three);
-//		slideTop.addTarget(R.id.image_four);
-//		transitionSet.addTransition(slideTop);
-//		Fade fade = new Fade();
-//		fade.excludeTarget(R.id.image_one, true);
-//		fade.excludeTarget(R.id.image_two, true);
-//		fade.excludeTarget(R.id.image_three, true);
-//		fade.excludeTarget(R.id.image_four, true);
-//		transitionSet.addTransition(fade);
-//		getWindow().setEnterTransition(transitionSet);
+		//		//资源文件指定过渡动画
+		//		getWindow().setEnterTransition(TransitionInflater.from(this).inflateTransition(R.transition.transition_target));
+		//代码制定过渡动画
+		TransitionSet transitionSet = new TransitionSet();
+		Slide slide = new Slide(Gravity.BOTTOM);
+		slide.addTarget(R.id.image_shared);
+		transitionSet.addTransition(slide);
+		Fade fade = new Fade();
+		fade.excludeTarget(android.R.id.statusBarBackground, true);
+		fade.excludeTarget(android.R.id.navigationBarBackground, true);
+		fade.excludeTarget(R.id.image_shared, true);
+		transitionSet.addTransition(fade);
+		transitionSet.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
+		getWindow().setEnterTransition(transitionSet);
 	}
 }
